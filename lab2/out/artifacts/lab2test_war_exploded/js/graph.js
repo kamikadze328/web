549ba4df7fced9b139f12704718382010487fc93
Sergey <48965524+kamikadze328@users.noreply.github.com>
2019-10-20 16:11:07 +0300

    canvas graph

--- lab2/out/artifacts/lab2test_war_exploded/js/graph.js
+++ lab2/out/artifacts/lab2test_war_exploded/js/graph.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
-
     const plot_canvas = document.getElementById("plot");
+
     const plot_context = plot_canvas.getContext("2d");
 
     plot_context.beginPath();
@@ -38,70 +38,60 @@ $(document).ready(function() {
     plot_context.closePath();
     plot_context.stroke();
 });
+
 function drawPoint(e) {
     const r = $('input[name=r_value]:checked', '#form').val();
-    if(r === undefined) {
+    if (r === undefined) {
         document.querySelector('#error-log').textContent =
             "Выберите значение r!";
     } else {
-        const cell = getCursorPosition(e);
+        const point = getCursorPosition(e);
         const plot_canvas = document.getElementById("plot");
         const plot_context = plot_canvas.getContext("2d");
         plot_context.beginPath();
-        plot_context.rect(x, y, 5, 5);
-        x = (x - 150)/100*r;
-        y = (-y + 150)/100*r;
-        alert(x + " " + y);
+        plot_context.rect(point.x, point.y, 5, 5);
+        point.x = (point.x - 150) /100*r;
+        point.y = (-point.y + 150) /100*r;
+        alert(point.x + " " + point.y);
         $.ajax({
-            type:'post',
-            url:'controller',//url адрес файла обработчика
-            data:{'x55':x, 'y':y, 'r':r},//параметры запроса
-            response:'text',//тип возвращаемого ответа text либо xml
-            error: function (message) {
-                console.log(message);
-                $('#results1').text(message);
-                alert("Error " + message);
-            },
-            success:function (data) {//возвращаемый результат от сервера
-                console.log(data);
-                //$('#results1').html(data);
-                /*$("#if").attr(
-                    "src", "data:text/html;charset=utf-8," + data
-                );*/
-                const ifr = document.getElementById('if').contentDocument;
-                ifr.open();
-                ifr.writeln(data);
-                ifr.close();
-
-                let stra = $('#results1').html(data);
-                stra = stra.text();
-                //stra = stra.substr(453);
-                const numy = stra.search("true");
-                if(numy !== -1) {
-                    plot_context.fillStyle = 'green';
-                } else {
-                    plot_context.fillStyle = 'red';
-                }
+            type: "POST",
+            url: "test",
+            data:
+                {
+                    x_value: point.x,
+                    y_value: point.y,
+                    r_value: r
+                },
+            success: data => {
+                document.querySelector('#ans').innerHTML = data;
+                //В следующей лабе надо будет точки двух цветов ставить => нужна серверная валидация
+                plot_context.fillStyle = 'green';
                 plot_context.fill();
-                //$('#results1').text(numy);
-                /*alert("Correct " + message);
-                $$('result',$$('result').innerHTML+'<br />'+data);*/
-            }
-        });
 
+            },
+            error: (jqXHR, textStatus, errorThrown) =>
+                document.querySelector('#error-log').innerHTML = "Ошибка HTTP: " + jqXHR.status + "(" + errorThrown + ")",
+            //elem.insertAdjacentHTML("beforeend", "") - будет получше, если по одной строке/точке высылать
+            dataType: "html"
+        });
     }
 }
+
 function getCursorPosition(e) {
+    let x;
+    let y;
+    const plot_canvas = document.getElementById("plot");
     if (e.pageX !== undefined && e.pageY !== undefined) {
         x = e.pageX;
         y = e.pageY;
-    }
-    else {
+    } else {
         x = e.clientX + document.body.scrollLeft +
             document.documentElement.scrollLeft;
         y = e.clientY + document.body.scrollTop +
             document.documentElement.scrollTop;
     }
-    x -= plot_canvas.offsetLeft ;
-    y -= plot_canvas.offsetTop ;
+    return {
+        x: x - plot_canvas.offsetLeft,
+        y: y - plot_canvas.offsetTop
+    }
 }
